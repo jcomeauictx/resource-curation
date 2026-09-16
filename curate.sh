b58516aa24b1d6037bdf43c9c9280f3e13c96022
John Comeau <jc@unternet.net>
2026-09-16 11:09:49 -0700

    only doing IPFS stuff so far, no actual curation

--- curate.sh
+++ curate.sh
@@ -3,14 +3,21 @@ verb=$1
 case $verb in
  "local")
   if [ "$2" ]; then
-   cid=$(ipfs add --offline $2)
+   cid=$(ipfs add --offline --quiet "$2")
+   echo added "$2" as $cid
   else
    echo Filename is a required argument >&2
   fi;;
  "remove")
   if [ "$2" ]; then
-   ipfs pin rm $2
+   ipfs pin rm "$2"
    ipfs repo gc
+  else
+   echo CID is a required argument >&2
+  fi;;
+ "publish")
+  if [ "$2" ]; then
+   ipfs dht provide "$2"
   else
    echo CID is a required argument >&2
   fi;;
